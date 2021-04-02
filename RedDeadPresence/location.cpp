65e9a97a3d56b66ec96962d606669b2e3a486da5
root-cause <32650536+root-cause@users.noreply.github.com>
2021-04-02 18:37:13 +0300

    Use label names instead of hash

--- RedDeadPresence/location.cpp
+++ RedDeadPresence/location.cpp
@@ -6,65 +6,65 @@ namespace presencemod::location
 	std::string getDistrictName(const Vector3& position)
 	{
 		// District names source: https://github.com/femga/rdr3_discoveries/tree/master/zones
-		// String hashes are from map.yldb
+		// Labels are from map.yldb
 		switch (ZONE::_GET_MAP_ZONE_AT_COORDS(position.x, position.y, position.z, static_cast<int>(ZoneType::DISTRICT)))
 		{
 			case joaat::generate("BayouNwa"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x86778A95);
+				return HUD::_GET_LABEL_TEXT("P_2_BAYOU_NWA");
 
 			case joaat::generate("bigvalley"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x171C4B1A);
+				return HUD::_GET_LABEL_TEXT("P_2_BIG_VALLEY");
 
 			case joaat::generate("BluewaterMarsh"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x93334804);
+				return HUD::_GET_LABEL_TEXT("P_2_BLUEWATER_MARSH");
 
 			case joaat::generate("ChollaSprings"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x11391C65);
+				return HUD::_GET_LABEL_TEXT("P_2_CHOLLA_SPRINGS");
 
 			case joaat::generate("Cumberland"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0xCA578D64);
+				return HUD::_GET_LABEL_TEXT("P_2_CUMBERLAND_FOREST");
 
 			case joaat::generate("DiezCoronas"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x1C5F6063);
+				return HUD::_GET_LABEL_TEXT("P_2_DIEZ_CORONAS");
 
 			case joaat::generate("GaptoothRidge"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0xD4024B27);
+				return HUD::_GET_LABEL_TEXT("P_2_GAPTOOTH_RIDGE");
 
 			case joaat::generate("greatPlains"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x2106539A);
+				return HUD::_GET_LABEL_TEXT("P_2_GREAT_PLAINS");
 
 			case joaat::generate("GrizzliesEast"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0xEF2B4071);
+				return HUD::_GET_LABEL_TEXT("P_2_GRIZZLIES_EAST");
 
 			case joaat::generate("GrizzliesWest"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0xA7038976);
+				return HUD::_GET_LABEL_TEXT("P_2_GRIZZLIES_WEST");
 
 			case joaat::generate("GuarmaD"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x27FC2128);
+				return HUD::_GET_LABEL_TEXT("P_1_GUARMA");
 
 			case joaat::generate("Heartlands"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x033F6174);
+				return HUD::_GET_LABEL_TEXT("P_2_THE_HEARTLANDS");
 
 			case joaat::generate("HennigansStead"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0xDCBBE18F);
+				return HUD::_GET_LABEL_TEXT("P_2_HENNIGANS_STEAD");
 
 			case joaat::generate("Perdido"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x1D6954B5);
+				return HUD::_GET_LABEL_TEXT("P_2_PERDIDO");
 
 			case joaat::generate("PuntaOrgullo"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x66A647D7);
+				return HUD::_GET_LABEL_TEXT("P_2_PUNTA_ORGULLO");
 
 			case joaat::generate("RioBravo"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x2F72C8B1);
+				return HUD::_GET_LABEL_TEXT("P_2_RIO_BRAVO");
 
 			case joaat::generate("roanoke"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x2E4CEDDE);
+				return HUD::_GET_LABEL_TEXT("P_2_ROANOKE_RIDGE");
 
 			case joaat::generate("scarlettMeadows"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0x88F8D1D0);
+				return HUD::_GET_LABEL_TEXT("P_2_SCARLETT_MEADOWS");
 
 			case joaat::generate("TallTrees"):
-				return HUD::GET_STRING_FROM_HASH_KEY(0xE09C9EC2);
+				return HUD::_GET_LABEL_TEXT("P_2_TALL_TREES");
 
 			default:
 				return "Unknown Location";
